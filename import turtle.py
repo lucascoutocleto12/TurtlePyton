9528266fa95b29f41359107bc6d19bcbffc21d4a
lucascoutocleto12 <lucascoutocleto12@gmail.com>
2022-04-26 16:08:56 -0300

    Envio arquivo Pyton

---
+++ import turtle.py
@@ -0,0 +1,18 @@
+import turtle
+
+turtle.speed(0)
+turtle.bgcolor("black")
+for i in range (15) :
+    for colours in ("red", "magenta", "blue", "cyan", "green", "yellow", "pink"):
+        turtle.color(colours)
+        turtle.pensize(3)
+        turtle.left(4)
+        turtle.forward(200)
+        turtle.left(90)
+        turtle.forward(200)
+        turtle.left(90)
+        turtle.forward(200)
+        turtle.left(90)
+        turtle.forward(200)
+        turtle.left(90)
+       
